7c10fc48e04a15970fbc8a75ab3d12457370837b
Nicolas Mossmann <nicolas.mossmann@etudiant.univ-reims.fr>
2023-02-15 15:36:02 +0100

    EventListener

--- src/EntityListener/BookmarkRateAverageUpdateListener.php
+++ src/EntityListener/BookmarkRateAverageUpdateListener.php
@@ -5,22 +5,51 @@ declare(strict_types=1);
 namespace App\EntityListener;
 
 use App\Entity\Rating;
-use Doctrine\ORM\Events;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\BookmarkRepository;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Events;
 
-#[AsEntityListener(
-    event: Events::prePersist,
-    entity: Rating::class
-)]
-class RatingSetUserListener
+class BookmarkRateAverageUpdateListener
 {
     public function __construct(private EntityManagerInterface $entityManager)
     {
     }
 
-    public function updateRateAverage(Rating $rating): void {}
-    public function postPersist(Rating $rating): void {}
-    public function postRemove(Rating $rating): void {}
-    public function postUpdate(Rating $rating): void  {}
+    public function updateRateAverage(Rating $rating): void
+    {
+        /**
+         * @var BookmarkRepository $rep
+         */
+        $rep = $this->entityManager->getRepository(BookmarkRepository::class);
+
+        $rep->updateRateAverage($rating->getBookmark()->getId());
+    }
+
+    #[AsEntityListener(
+        event: Events::postPersist,
+        entity: Rating::class
+    )]
+    public function postPersist(Rating $rating): void
+    {
+        $this->updateRateAverage($rating);
+    }
+
+    #[AsEntityListener(
+        event: Events::postRemove,
+        entity: Rating::class
+    )]
+    public function postRemove(Rating $rating): void
+    {
+        $this->updateRateAverage($rating);
+    }
+
+    #[AsEntityListener(
+        event: Events::postUpdate,
+        entity: Rating::class
+    )]
+    public function postUpdate(Rating $rating): void
+    {
+        $this->updateRateAverage($rating);
+    }
 }
